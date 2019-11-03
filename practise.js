e0149d4da3ed7888598db414c646641d94db9f71
mrNicklaus <scriptyacademy@gmail.com>
2019-11-03 15:45:13 +0300

    Operation TicTac

--- practise.js
+++ practise.js
@@ -2,7 +2,7 @@ var x = 1;
 
 while(x<=20){
     if(x%3 ===0 && x%5 ===0){
-        console.log("JuliaJames");
+        console.log("TicTac");
     }else if(x%3 ===0){
         console.log("Tic")
     }else if(x%5 ===0){
